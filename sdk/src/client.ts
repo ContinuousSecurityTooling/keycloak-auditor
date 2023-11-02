8e3e24ed14ce028514a221e53be642f50d4fab20
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-11-02 06:26:34 +0100

    chore: Remove unused imports

--- sdk/src/client.ts
+++ sdk/src/client.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import { AuditedClientRepresentation, AuditedUserRepresentation, ClientLoginDetails, UserLoginDetails } from './spi.js';
+import { AuditedClientRepresentation, AuditedUserRepresentation } from './spi.js';
 
 export interface Credentials {
   grantType: string;

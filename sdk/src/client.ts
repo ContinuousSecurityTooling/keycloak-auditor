59af16111038074968b06605c32be130b6f5ce6a
Martin Reinhardt <contact@martinreinhardt-online.de>
2026-04-23 07:34:05 +0200

    fix(npm): Correcting changed generated spi name

--- sdk/src/client.ts
+++ sdk/src/client.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import { AuditedClientRepresentation, AuditedUserRepresentation } from './spi.js';
+import { AuditedClientRepresentation, AuditedUserRepresentation } from './keycloak-auditor-spi.js';
 
 export interface Credentials {
   grantType: string;
